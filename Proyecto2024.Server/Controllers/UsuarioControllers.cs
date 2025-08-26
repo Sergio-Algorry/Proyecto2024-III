54cecd53aa2fbff0bd1680420a36a9518536f7de
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2025-08-26 09:40:53 -0300

    Comenzando con los controllers

---
+++ Proyecto2024.Server/Controllers/UsuarioControllers.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto2024.Shared.DTO;
+
+namespace Proyecto2024.Server.Controllers
+{
+    [ApiController]
+    [Route("api/usuarios")]
+    public class UsuarioControllers : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public UsuarioControllers(UserManager<IdentityUser> userManager,
+                         SignInManager<IdentityUser> signInManager,
+                         IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("crear")]
+        public async Task<ActionResult<UserTokenDTO>> CreateUser([FromBody] UserInfoDTO model)
+        {
+            var usuario = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var resultado = await userManager.CreateAsync(usuario, model.Password);
+
+            if (resultado.Succeeded)
+            {
+                return await BuildToken(model);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors.First());
+            }
+        }
+
+    }
+
+}
--- Proyecto2024.Server/Program.cs
+++ Proyecto2024.Server/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Proyecto2024.BD.Data;
@@ -35,6 +36,21 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<Context>()
                 .AddDefaultTokenProviders();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(builder.Configuration["jwtkey"]))
+        };
+    });
 
 builder.Services.AddAutoMapper(typeof(Program));
 
