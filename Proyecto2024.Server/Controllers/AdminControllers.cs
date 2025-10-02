ce4aa5b98fe99d72f079b12a2cfdcb953541e813
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2025-10-02 11:32:49 -0300

    listado de usuarios

---
+++ Proyecto2024.Server/Controllers/AdminControllers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto2024.BD.Data;
+using Proyecto2024.Shared.DTO;
+
+namespace Proyecto2024.Server.Controllers
+{
+    [ApiController]
+    [Route("usuarios/admin")]
+    //Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "admin")]
+    public class AdminControllers : ControllerBase
+    {
+        private readonly Context context;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AdminControllers(Context context,
+                                UserManager<IdentityUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserListadoDTO>>> Get()
+        {
+            var usuarios = context.Users.AsQueryable();
+            var usuariosDTO = await usuarios.Select(u => new UserListadoDTO
+            {
+                Id = u.Id,         
+                Email = u.Email!
+            }).ToListAsync();
+            return usuariosDTO;
+        }
+    }
+}
---
+++ Proyecto2024.Shared/DTO/RolDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2024.Shared.DTO
+{
+    public class RolDTO
+    {        
+        public string Nombre { get; set; } = null!;
+    }
+}
